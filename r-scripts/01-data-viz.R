7b1a726833cbe4dad750ce5f7665344a244b5dd1
lruolin <elau85@yahoo.com>
2024-09-14 15:25:38 +0800

    completed plots until section 1.4

--- r-scripts/01-data-viz.R
+++ r-scripts/01-data-viz.R
@@ -28,7 +28,7 @@ ggthemr("fresh")
 
 datatable(penguins)
 
-## There are 8 columns
+## There are 8 columns, 344 rows
 glimpse(penguins)
 
 ## Includes measurements for penguin species, island in 
@@ -48,6 +48,145 @@ penguins %>%
   geom_smooth(method = 'lm')  + # based on linear model
   labs(
     title = "Body mass and flipper length",
-    caption = "Source: Palmer Penguins Dataset"
+    caption = "Source: palmerpenguin package"
   ) +
   scale_color_colorblind()
+
+
+## Make a scatterplot of bill_depth_mm vs. bill_length_mm. 
+## That is, make a scatterplot with bill_depth_mm on the y-axis 
+## and bill_length_mm on the x-axis. 
+## Describe the relationship between these two variables.
+
+glimpse(penguins)
+
+penguins %>% 
+  ggplot(aes(
+    x = bill_length_mm,
+    y = bill_depth_mm
+  )) +
+  geom_point(aes(col = species, 
+                 shape = species
+                 ),
+             na.rm = T) +
+  geom_smooth(method = 'lm') +
+  labs(title = "Bill Depth vs Bill Length",
+       subtitle = "As bill length increases, bill depth decreases",
+       caption = "Source: palmerpenguin package") +
+  scale_color_colorblind()
+
+
+## Visulize body mass vs flipper length
+## Map at geom level
+penguins %>% 
+  ggplot(
+    aes(x = flipper_length_mm,
+        y = body_mass_g)
+  ) +
+  geom_point(aes(col = bill_depth_mm)) +
+  geom_smooth()
+
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = flipper_length_mm, 
+    y = body_mass_g, 
+    color = island)
+) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
+# compare with this -> they are the same
+## defining at global and local level
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point() +
+  geom_smooth()
+
+ggplot() +
+  geom_point(
+    data = penguins,
+    mapping = aes(
+      x = flipper_length_mm, y = body_mass_g)
+  ) +
+  geom_smooth(
+    data = penguins,
+    mapping = aes(
+      x = flipper_length_mm, y = body_mass_g)
+  )
+
+
+## categorical vs numerical -----
+
+### count plot -----
+penguins %>% 
+  ggplot(aes(x = species)) +
+  geom_bar()
+
+### reordering by frequency ----
+penguins %>% 
+  ggplot(aes(x = fct_infreq(species))) +
+  geom_bar()
+
+# fill changes the shape filled
+penguins %>% 
+  ggplot(aes(x = fct_infreq(species))) +
+  geom_bar(fill = 'grey50') +
+  labs(title = "Count of penguins for each species",
+       caption = "Source: palmerpenguins package")
+
+# col only changes the outline
+penguins %>% 
+  ggplot(aes(x = fct_infreq(species))) +
+  geom_bar(col = 'purple')
+
+### histogram -----
+penguins %>% 
+  ggplot(aes(
+    x = body_mass_g
+  )) +
+  geom_histogram(binwidth = 200) +
+  labs(
+    title = "Histogram for body_mass",
+    subtitle = "bin width of 200 was used for histogram",
+    caption = "Source: palmerpenguins package"
+  )
+
+### density plot -----
+penguins %>% 
+  ggplot(
+    aes(x = body_mass_g)
+  ) +
+  geom_density() +
+  labs(
+    title = "Density plot for body_mass",
+    subtitle = "Can see shape of distribution with a density plot too.",
+    caption = "Source: palmerpenguins package"
+  )
+
+
+### diamonds dataset -----
+glimpse(diamonds)
+summary(diamonds)
+
+diamonds %>% 
+  ggplot(aes(x = carat)) +
+  geom_histogram(binwidth = 10)
+
+diamonds %>% 
+  ggplot(aes(x = carat)) +
+  geom_histogram(binwidth = 0.7)
+
+diamonds %>% 
+  ggplot(aes(x = carat)) +
+  geom_histogram(binwidth = 0.07)
+
+diamonds %>% 
+  ggplot(aes(x = carat)) +
+  geom_histogram(binwidth = 0.2) +
+  scale_x_continuous(n.breaks = 10) +
+  labs(title = "Distribution of carats in diamonds",
+       subtitle = "Using binwidth = 0.2. Most of the diamonds have carat size of about 0.3-0.7")
