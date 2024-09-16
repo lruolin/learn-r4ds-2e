00165a39efe0818a3b979afab41ed1e66c3164dd
lruolin <elau85@yahoo.com>
2024-09-16 10:27:14 +0800

    completed chap 1

--- r-scripts/01-data-viz.R
+++ r-scripts/01-data-viz.R
@@ -11,7 +11,9 @@ p_load(tidyverse, conflicted,
        palmerpenguins,
        ggthemes,
        ggthemr,
-       DT
+       DT,
+       patchwork, # to patch figures
+       here # to save plots to figures
        )
 
 conflicts_prefer(dplyr::filter)
@@ -190,3 +192,131 @@ diamonds %>%
   scale_x_continuous(n.breaks = 10) +
   labs(title = "Distribution of carats in diamonds",
        subtitle = "Using binwidth = 0.2. Most of the diamonds have carat size of about 0.3-0.7")
+
+### boxplot -----
+penguins %>% 
+  ggplot(aes(x = species, y = body_mass_g)) +
+  geom_boxplot() +
+  labs(title = "Boxplot showing distribution of body mass for each species",
+       caption = "Source: palmerpenguins package")
+
+
+### density plot
+penguins %>% 
+  ggplot(aes(x = body_mass_g, 
+             fill = species,
+             color = species)) +
+  geom_density(alpha = 0.5) +
+  labs(title = "Density plot showing distribution of body mass for each species",
+       caption = "Source: palmerpenguins packge")
+
+
+### stacked bar plots -----
+penguins %>% 
+  ggplot(aes(x = island, fill = species)) +
+  geom_bar() +
+  labs(title = "Stacked bar plot tells us the frequency count of species per island",
+       caption = "Source: palmerpenguins package")
+
+
+### relative count
+penguins %>% 
+  ggplot(aes(x = island, fill = species)) +
+  geom_bar(position = 'fill') +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "Relative count of penguins on each island",,
+       subtitle = "Gentoo penguins all live on Biscoe. Chinstrap all live on Dream island.",
+       caption = "Source: palmerpenguins package") 
+
+### scatterplots -----
+glimpse(penguins)
+penguins %>% 
+  ggplot(aes(flipper_length_mm, body_mass_g)) +
+  geom_point() +
+  labs(title = "Visualizing relationship between flipper length and body mass",
+       caption = "Source: palmerpenguins package")
+
+
+### three or more variables ----
+penguins %>% 
+  ggplot(aes(x = flipper_length_mm,
+             y = body_mass_g)) +
+  geom_point(aes(col = species)) +
+  labs(title = "Relationship between body mass and flipper length, colored by penguin species",
+       subtitle = "Gentoo penguins have larger body mass and longer flipper length",
+       caption = "Source: palmerpenguins package")
+
+## faceting
+penguins %>% 
+  ggplot(aes(x = flipper_length_mm,
+             y = body_mass_g)) +
+  geom_point(aes(col = species)) +
+  facet_wrap(~island) +
+  labs(title = "Relationship between body mass and flipper length, colored by penguin species",
+       caption = "Source: palmerpenguins package")
+
+
+## mpg dataset -----
+?mpg
+glimpse(mpg)
+
+## categorical variables:
+## manufacturer, model, year, cyl, trans, drv, fl, class
+
+## numerical variables:
+## displ, cty, hwy
+
+mpg %>% count(manufacturer)
+mpg %>% count(model)
+
+### scatterplot: hwy vs dipl ----
+p1 <- 
+  mpg %>% 
+  ggplot(aes(x = displ,
+             y = hwy)) +
+  geom_point(aes(size = cty))
+
+p2 <- 
+  mpg %>% 
+  ggplot(aes(x = displ,
+             y = hwy)) +
+  geom_point(aes(col = cty))
+
+p1+p2
+
+
+## penguins ----
+penguins %>% 
+  ggplot(aes(x = bill_depth_mm,
+             y = bill_length_mm)) +
+  geom_point(aes(col = species))
+
+penguins %>% 
+  ggplot(aes(x = bill_depth_mm,
+             y = bill_length_mm)) +
+  geom_point(aes(col = species)) +
+  facet_wrap(~species)
+
+
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = bill_length_mm, y = bill_depth_mm, 
+    color = species, shape = species
+  )
+) +
+  geom_point() +
+  labs(color = "Species", shape = "Species")
+
+# SAVING PLOTS -----
+
+fig_folder <- 
+  here("figures",
+       "chap-01")
+
+fig_folder
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point()
+
+ggsave(filename = here(fig_folder, "penguin-plot.png"))
